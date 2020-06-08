a0c9b0200b93997c224f227f58344b8cf63b43b8
greyspurv2 <35030409+greyspurv2@users.noreply.github.com>
2020-06-08 06:25:41 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Remotify"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+ 
+}
+</style>
---
+++ index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class=" index container">
+        <div class="card" v-for="job in jobs" :key"job.id">
+            <div class="Card-content">
+                <h2 class="indigo-text"> {{ job.title }}</h2>
+                    <ul class="Skills">
+                        <li v-for="(skill, index) in skill.skills" :key"index">
+                            <span class="chip"> {{skill}} </span>
+                                </li>
+                                </ul>
+                                </div>
+                                </div>
+                                </div>
+                            </template>
+
+                         <script>
+                            export default {
+                                name: 'Index',
+                                data () {
+                                    return {
+                                        jobs: [
+                                         {[title: 'webdev', slug: 'web', skills: ['HTML', 'CSS', 'JS', 'VUE']}
+                                         { [title: 'DevOps', slug: 'Ops', skills: ['Chef', 'Docker', 'AWS']}
+                                         { [title: 'Backend', slug: 'backend', skills: ['NodeJS', 'Docker', 'AWS']}
+                                         { [title: 'DevOps', slug: 'Ops', skills: ['Chef', 'Docker', 'AWS']}
+                                          { [title: 'DevOps', slug: 'Ops', skills: ['Chef', 'Docker', 'AWS']}
+
+                                    }
+                                }
+                            }
+                        </script>
